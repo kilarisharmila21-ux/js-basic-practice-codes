dbfc00a14696ca94a3a36c583c2e4bd0a04b43ba
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-07 00:09:26 +0530

    A operators built using JS.

---
+++ operators2.js
@@ -0,0 +1,45 @@
+// Unary operator
+
+let x = 5;
+console.log(++x);
+
+// Post Increment
+
+let a = 5;
+console.log(a++);
+console.log(a);
+
+// Post Decrement
+
+let b = 10;
+
+console.log(b--);
+console.log(b);
+
+// Type operator (typeof)
+
+let name = "Chinnu";
+
+console.log(typeof num);
+
+//Ternary operator: Shortcut for ifelse
+
+let age = 20;
+let result = age>=18?"Adult": "Minor";
+
+console.log(result);
+
+// Largest number
+
+let p = 10;
+let q = 20;
+
+let largest = p>q? p:q;
+
+console.log(largest);
+
+// Logical NOT
+
+let isLoggedIn = false;
+
+console.log(isLoggedIn);

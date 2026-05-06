9b615aa8c3ebce563843450964463d9b80114bb5
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-07 00:07:28 +0530

    A variables built using JS.

---
+++ variables.js
@@ -0,0 +1,120 @@
+// let variable
+
+let name = "Chinnu";
+let age = 20;
+
+console.log(name);
+console.log(age);
+
+// let variable 2
+
+let name2 = "Chinnu";
+console.log("My name is +name");
+
+// let variable 3
+
+let age2 = 20;
+console.log("My age is" +age);
+
+// sum variable
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2
+console.log("Sum is" +sum);
+
+// Rectangle area
+
+let length = 10;
+let width = 20;
+
+let area = length * width;
+
+console.log("Area is" +area);
+
+// change variable value
+
+let number = 10;
+
+console.log(number);
+
+number = 20;
+
+console.log(number);
+
+// Favourite color
+
+let color = "Blue";
+
+console.log("My favourite color is" +color);
+
+// constant 
+
+const pi = 3.14;
+
+console.log("Value of pi is" +pi);
+
+// Combine variables
+
+let name3 = "Chinnu";
+let age3 = 20;
+let city = "Tirupati";
+
+console.log("My name is" +name);
+console.log("I am" +age +"years old");
+console.log("I live in" +city);
+
+// Multiple variables in one line
+
+let a=10, b=20, c=30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Copy value from another variable
+
+let x = 10;
+let y = x;
+
+console.log(y);
+
+// Using template strings (modern way)
+
+let name4 = "Chinnu";
+let age4 = 20;
+
+console.log(`My name is ${name} and I am ${age} years old`);
+
+// Update variable using itself
+
+let count = 5
+
+count = count+1
+
+console.log(count+1);
+
+//String variables
+
+let name5 = "Chinnu";
+
+console.log(name);
+
+// Undefined
+
+let p;
+
+console.log(p);
+
+// Swap variables
+
+let u = 10;
+let v = 20;
+
+let temp = u;
+u = v;
+v = temp;
+
+console.log(u);
+console.log(v);

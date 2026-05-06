c7718510d1a74d5982ea6275e2d9412b6e927c51
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-07 00:08:02 +0530

    A operators built using JS.

---
+++ operators.js
@@ -0,0 +1,105 @@
+// Arithmetic operator
+
+let a = 10;
+let b = 3;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+// Assignment operator
+
+let x = 10;
+
+x +=5; // x = x + 5
+console.log(x);
+
+// Comparison operator
+
+let p = 10;
+let q = 10;
+
+console.log(p==q);
+console.log(p===q);
+
+// Logical operator
+
+let age = 20;
+let hasID = true;
+
+console.log(age > 18 && hasID);
+
+// Increment operator
+
+let m = 5;
+
+m++; //6
+m--; //5
+
+// All operators
+
+let a1 = 10;
+let b1 = 5;
+
+console.log(a+b);
+console.log(a>b);
+console.log(a==b);
+
+// Assignment operator 2
+
+let x1 = 10;
+x+=5;
+console.log(x);
+
+// Equal operator
+
+let a2 = 10;
+let b2 = 10;
+console.log(a==b);
+
+// Substract operator
+
+let x2 = 15;
+x2-=3;
+console.log(x);
+
+// Strict equal operator
+
+let a3 = 10;
+let b3 = 10;
+
+console.log(a===b);
+
+// Greater than
+
+let a4 = 15;
+let b4 = 10;
+
+console.log(a>b);
+
+// Less than operator
+
+let a5 = 5;
+let b5 = 8;
+
+console.log(a<b);
+
+// Logical AND
+
+let age2 = 20;
+let hasID2 = true;
+
+// Logical OR 
+
+let age3 = 16;
+let hasID3 = true;
+
+console.log(age>18 / hasID);
+
+// Increment operator 2
+
+let x3 = 5;
+x++;
+console.log(x);

bc8bde8563a35d2b96b00ffde6464f9b963d4ac3
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-07 00:10:42 +0530

    A non primitive data types built using JS.

---
+++ nonprimitivedatatype.js
@@ -0,0 +1,22 @@
+// Object data type
+
+let student = {
+    name: "Chinnu",
+    age: 20,
+    city: "Chennai",
+}
+
+console.log(student .name);
+
+// Array data type
+
+let fruits = ["Apple", "Mango", "Banana"];
+
+console.log(fruits[0]);
+
+// Function data type
+
+function greet () {
+    console.log("Hello");
+}
+greet();

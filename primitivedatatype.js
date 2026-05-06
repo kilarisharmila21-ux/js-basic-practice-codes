24972ce138858d502a4aaad196afb100c5daed29
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-07 00:10:06 +0530

    A primitive data types built using JS.

---
+++ primitivedatatype.js
@@ -0,0 +1,38 @@
+// String data type
+
+let name = "Chinnu";
+let city = "Chennai";
+
+console.log(typeof name);
+
+// Number data type
+
+let age = 20;
+let price = 99.99;
+
+console.log(typeof age);
+
+// Boolean data type
+
+let isStudent = true;
+let isLoggedIn = false;
+
+console.log(typeof isStudent);
+
+// Undefined data type
+
+let x;
+console.log(x);
+
+// Null data type
+
+let data = null;
+
+// BigInt data type
+
+let bigNumber = 1234567891234567890n;
+
+// Symbol data type
+
+let id = Symbol("id");
+
